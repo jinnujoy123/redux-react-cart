3efeb55c6c474bffbe30c1be3c2114fd144952cd
Jinnu <jinnujoy94@gmail.com>
2025-09-19 08:57:22 +0530

    url updated

--- src/redux/slices/productSlice.js
+++ src/redux/slices/productSlice.js
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetchAllProducts=createAsyncThunk("products/fetchAllProducts",async()=>{
-    const response=await axios.get("http://dummyjson.com/products")
+    const response=await axios.get("https://dummyjson.com/products")
     // console.log(response.data.products);
     sessionStorage.setItem("allProducts",JSON.stringify(response.data.products))
     return response.data.products
